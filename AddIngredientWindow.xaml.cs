f9f52b72ab51a7306a921fb6de90b4c464ad8633
Adam Njegovanovic <adamnj@kth.se>
2023-05-05 13:03:30 +0200

    Fix so you cant add an ingredient without all fields filled in and amount must be a number

--- AddIngredientWindow.xaml.cs
+++ AddIngredientWindow.xaml.cs
@@ -28,9 +28,19 @@ namespace ProjInda_Recipe_Manager
         public decimal IngredientAmount { get; set; }
         public string IngredientUnit { get; set; }
         private void btnOK_Click(object sender, RoutedEventArgs e)
-        {
+        {   
+            if(txtName.Text == "" || txtAmount.Text == "" || txtUnit.Text == "")
+            {
+                MessageBox.Show("Please fill in all fields");
+                return;
+            }
             IngredientName = txtName.Text;
-            IngredientAmount = decimal.Parse(txtAmount.Text);
+            bool isNumeric = decimal.TryParse(txtAmount.Text, out decimal number);
+            if(!isNumeric ) {
+                MessageBox.Show("Amount must be a number");
+                return;
+            }
+            IngredientAmount = number;
             IngredientUnit = txtUnit.Text;
             this.DialogResult = true;
             this.Close();
